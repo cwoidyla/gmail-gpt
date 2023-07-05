355d0eaea76647c13ce5534485ac031b4e43df42
conrad <conrad@MacBook-Pro-2.local>
2023-07-05 16:37:03 -0700

    prompt: in , elaborate on python3 code to get inbox emails from the last 1 day

--- controller.py
+++ controller.py
@@ -5,6 +5,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 import openai
+from googleapiclient.errors import HttpError
+from base64 import urlsafe_b64decode
+from email import policy
+from email.parser import BytesParser
+from datetime import datetime, timedelta
+import time
 
 # 1. Login to Gmail API
 def gmail_login():
@@ -23,9 +29,35 @@ def gmail_login():
 
 # 2. Fetch and Preprocess Gmail Data
 def fetch_and_preprocess(gmail):
-    # Logic to fetch and preprocess Gmail data goes here.
-    print("in fetch_and_preprocess()")
-    pass
+    try:
+        after = (datetime.now() - timedelta(days=1)).strftime('%Y/%m/%d')
+        timestamp = int(time.mktime(datetime.strptime(after, "%Y/%m/%d").timetuple()))
+        query = f'after:{timestamp}'
+
+        # Get emails from the last 30 days
+        results = gmail.users().messages().list(userId='me', q=query).execute()
+        messages = results.get('messages', [])
+
+        gmail_data = []
+        for message in messages:
+            msg = gmail.users().messages().get(userId='me', id=message['id'], format='raw').execute()
+            msg_str = urlsafe_b64decode(msg['raw']).decode()
+            mime_msg = BytesParser(policy=policy.default).parsebytes(msg_str.encode())
+
+            data = {
+                'subject': mime_msg['subject'],
+                'from': mime_msg['from'],
+                'to': mime_msg['to'],
+                'date': mime_msg['date'],
+                'body': mime_msg.get_body().get_content()
+            }
+
+            gmail_data.append(data)
+
+        return gmail_data
+
+    except HttpError as error:
+        print(f'An error occurred: {error}')
 
 # 3. Use GPT-4 to process data
 def process_with_gpt4(gmail_data):
@@ -42,6 +74,7 @@ def rank_emails(processed_data):
 def main():
     gmail = gmail_login()
     gmail_data = fetch_and_preprocess(gmail)
+    print(gmail_data)
     processed_data = process_with_gpt4(gmail_data)
     ranked_emails = rank_emails(processed_data)
 
